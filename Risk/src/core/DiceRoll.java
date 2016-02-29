023d17b85588651c178ad34320a2b57d20eae5ac
Gregory <gregorysloggett@gmail.com>
2016-02-29 13:21:01 +0000

    Dice Roll class changed to have dice rolls for attacker and defender. Only printing on the command line currently. 2 rolls for the defender and 3 for the attacker. Attacker must roll higher in order to win combat, if defender rolls higher or the same as the attacker then he/she wins.
    Signed-off-by: Gregory <gregorysloggett@gmail.com>

--- Risk/src/core/DiceRoll.java
+++ Risk/src/core/DiceRoll.java
@@ -5,6 +5,7 @@
  * 
  */
 package core;
+
 class Dice {
 	private int faceValue;
 
@@ -24,16 +25,22 @@ class Dice {
 public class DiceRoll {
 	
 	static int i;
-	String string;
+	static String string;
+	
 	private static Dice Player1Roll = new Dice();
 	private static Dice Player2Roll = new Dice();
+
+	private static Dice AttackerRoll = new Dice();
+	private static Dice DefenderRoll = new Dice();
+
+	private static int[] AttackerRolls = new int[3];
+	private static int[] DefenderRolls = new int[2];
+	
+	private static int attackerHighestRoll;
+	private static int defenderHighestRoll;
 	
 	public void rollDice(){
-			
-			Player1Roll = new Dice();
 			Player1Roll.roll();
-		
-			Player2Roll = new Dice();
 			Player2Roll.roll();
 	}
 	
@@ -49,10 +56,74 @@ public class DiceRoll {
 		}	
 	}
 	
-	public int getPlayer1RollValue(){
+	public static int getPlayer1RollValue(){
 		return Player1Roll.value();
 	}
-	public int getPlayer2RollValue(){		
+	public static int getPlayer2RollValue(){		
 		return Player2Roll.value();
+	}
+	
+	public static int getAttackerRollValue(){
+		return AttackerRoll.value();
+	}
+	public static int getDefenderRollValue(){
+		return DefenderRoll.value();
+	}
+	
+	public static void combatDiceRoll(){
+		for(i=0; i<3; i++){
+			AttackerRoll.roll();		
+			AttackerRolls[i] = getAttackerRollValue();
+			System.out.println("Player 1 rolls = " + AttackerRolls[i]);
+			
+			if(i==0){
+				attackerHighestRoll = AttackerRolls[i];
+			}
+			if(i>0 &&(AttackerRolls[i] > attackerHighestRoll)){
+				attackerHighestRoll = AttackerRolls[i];
+			}
+		}
+		System.out.println("");
+		for(i=0; i<2; i++){
+			DefenderRoll.roll();
+			DefenderRolls[i] = getDefenderRollValue();
+			System.out.println("Player 2 rolls = " + DefenderRolls[i]);
+			
+			if(i==0){
+				defenderHighestRoll = DefenderRolls[i];
+			}
+			if((i>0) && (DefenderRolls[i] > defenderHighestRoll)){
+				defenderHighestRoll = DefenderRolls[i];
+			}
+		}
+
+		System.out.println("Player 1 highest roll = " + attackerHighestRoll);
+		System.out.println("Player 2 highest roll = " + defenderHighestRoll);
+		
+		combatDiceRollWinner();
+		
+		if(string == "0"){
+			System.out.println("Player 1 wins combat");
+		}
+		if(string == "1"){
+			System.out.println("Player 2 wins combat");
+		}
+	}
+	
+	public static String combatDiceRollWinner(){
+		if(attackerHighestRoll > defenderHighestRoll){
+			string = "0";
+		}
+		if(attackerHighestRoll <= defenderHighestRoll){
+			string = "1";
+		}
+		return string;
+	}
+	
+	public static int getAttackerHighestRoll(){
+		return attackerHighestRoll;
+	}
+	public static int getDefenderHighestRoll(){
+		return defenderHighestRoll;
 	}
 }
