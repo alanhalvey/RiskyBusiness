5c9cf038bb82cd5dc6157d98f39a2c50c54c68cc
Gregory <gregorysloggett@gmail.com>
2016-03-28 17:09:38 +0100

    CommandInput class commented and tidied slightly...
    Signed-off-by: Gregory <gregorysloggett@gmail.com>

--- Risk/src/core/CommandInput.java
+++ Risk/src/core/CommandInput.java
@@ -40,13 +40,14 @@ public class CommandInput extends JPanel{
 	static JTextPane outputWindow = new JTextPane();
 
 	static String currentPlayer = "";
-	public static String player1 = "";
-	public static String player2 = "";
+	public static String player1 = ""; //Holds player 1 name
+	public static String player2 = ""; //Holds player 2 name
 
 	static StyledDocument doc = outputWindow.getStyledDocument();
 	static Style style = outputWindow.addStyle("Style", null);
 	static String checkIfDieEqual = "NO";
 
+	//Holds colours for players
 	public static Color player1Colour = Color.BLUE;
 	public static Color player2Colour = Color.MAGENTA;
 	public static Color currentPlayerColour = Color.BLACK;
@@ -78,17 +79,14 @@ public class CommandInput extends JPanel{
 		scrollPane.setPreferredSize(new Dimension(250, 480));
 		scrollPane.setMinimumSize(new Dimension(10, 10));
 
-		/*
-		 * The above is setting up the scroll bar and adding the output window to it
-		 */
+		// The above is setting up the scroll bar and adding the output window to it 
 
 		DefaultCaret caret = (DefaultCaret)outputWindow.getCaret();
 		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 
-		/*
-		 * Adding the button and the action listener to the enter key and
-		 * th update caret follows the scroll barto the bottom of the screen
-		 */
+		// Adding the button and the action listener to the enter key and 
+		// the update caret follows the scroll bar to the bottom of the screen
+		 
 
 		JPanel finishedPanel = new JPanel();
 
@@ -96,25 +94,24 @@ public class CommandInput extends JPanel{
 		finishedPanel.add(scrollPane, BorderLayout.SOUTH);
 		finishedPanel.add(enterButton, BorderLayout.EAST);
 		finishedPanel.add(commandInputWindow);
-		/*
-		 * adds all elements to the final panel and gets it ready to be aded to the frame in main.
-		 */
+		
+		//adds all elements to the final panel and gets it ready to be aded to the frame in main.
 		add(finishedPanel);
 
 		return;
 	}
 
-	public static void run(){
+	public static void run(){ //runs the start of the game, usernames entered, first player to go decided using DiceRoll class.
 		appendStringTo("Enter Username for Player 1: \n", Color.BLACK);
 
 		while(player1.length() < 3 || player1.length() > 10){
-			ErrorHandling.Player1UsernameChecks();
+			ErrorHandling.Player1UsernameChecks(); //Error handling checks in ErrorHandling class to keep commandInput tidy
 		}
 
 		appendStringTo("Enter Username for Player 2: \n", Color.BLACK);	
 
 		while((player2.length() < 3 || player2.length() > 10)){
-			ErrorHandling.Player2UsernameChecks();
+			ErrorHandling.Player2UsernameChecks(); //Error handling checks in ErrorHandling class to keep commandInput tidy
 			if(player2.equalsIgnoreCase(player1)){
 				appendStringTo("That username has already been taken, please enter another name: \n", Color.RED);
 				player2 = "";
@@ -122,7 +119,7 @@ public class CommandInput extends JPanel{
 		}
 	}
 
-	public static String getCommand() {
+	public static String getCommand() { //This function waits for the user to enter a string before taking the result in the text field
 		String command;
 		synchronized (commandBuffer) {
 			while (commandBuffer.isEmpty()) {
@@ -134,9 +131,10 @@ public class CommandInput extends JPanel{
 			}
 			command = commandBuffer.pop();
 		}
-		return command;
+		return command; //returns the user input (very important funcion)
 	}
-	public static void updatePlayerColour(String currentPlayer){
+	
+	public static void updatePlayerColour(String currentPlayer){ //updates the current player colour in sync with the current player updation
 		if(currentPlayer == player1){
 			currentPlayerColour = player1Colour;
 		}
@@ -145,6 +143,7 @@ public class CommandInput extends JPanel{
 		}
 	}
 
+	//Function to place armies/reinforcements at the start of the game
 	public static void placeUnits(String currentPlayer) {
 
 		updatePlayerColour(currentPlayer);
@@ -156,10 +155,7 @@ public class CommandInput extends JPanel{
 			currentUnits = Data.PLAYER_2_ARMIES;
 		}
 
-
-
-
-		if(currentPlayer.compareTo(player1)==0 && currentUnits ==0 ){
+		if(currentPlayer.compareTo(player1)==0 && currentUnits ==0 ){ //if its player one and he/she has no units left ..
 			currentPlayer=player2;
 			placeUnits(currentPlayer);
 			currentUnits = Data.PLAYER_2_ARMIES;
@@ -170,7 +166,7 @@ public class CommandInput extends JPanel{
 				placeUnits(currentPlayer);
 			}
 		}
-		else if(currentPlayer.compareTo(player2)==0 && currentUnits == 0){
+		else if(currentPlayer.compareTo(player2)==0 && currentUnits == 0){ //if its player two and he/she has no units left ..
 			currentPlayer=player1;
 			currentUnits = Data.PLAYER_1_ARMIES;
 			if(currentUnits == 0){
@@ -180,17 +176,16 @@ public class CommandInput extends JPanel{
 				placeUnits(currentPlayer);
 			}
 		}
-
-
 		else if(currentUnits!=0){
 			appendStringTo(currentPlayer + ", please type the country name to place units in: \n", Color.BLACK);
+			
 			if(Data.PLAYER_1_ARMIES != 0){
 				if(currentPlayer.compareTo(player1)==0){
 					String country = getCommand();
 					ErrorHandling.P1checkTerritories(country);
 				}
 			}
-
+			
 			if(Data.PLAYER_2_ARMIES != 0){
 				if(currentPlayer.compareTo(player2)==0){
 					String country = getCommand();
@@ -198,20 +193,18 @@ public class CommandInput extends JPanel{
 				}
 			}	
 		}
-
-
 	}
 
+	//Function places the neutrals armies
 	public static void placeNeutrals(String currentPlayer, String Neutral) {
 		appendStringTo(currentPlayer + ", which " + Neutral+ " country do you want to place units in\n", Color.BLACK);
 		String country = getCommand();
 		while(Data.neutralsFilled==false){
 			NeutralChecks(country, Neutral);
 		}
-
-
 	}
 
+	//Function allows player 1 & 2 to assign neutral armies as specified in the brief for sprint 3
 	private static void NeutralChecks(String country, String currentNeutral) {
 		for(int i=0;i<42;i++){
 			if (country.compareToIgnoreCase(Deck.countriesAfterShuffle[i].getName())==0 
@@ -223,6 +216,7 @@ public class CommandInput extends JPanel{
 					int numReinforcementsToPlace = Integer.parseInt(numToPlace);
 					int currentNeutralArmies = 0;
 
+					//current neutral armies variable is set based on which neutral is the current neutral!
 					if(currentNeutral.compareToIgnoreCase("Neutral 1")==0){
 						currentNeutralArmies = Data.NEUTRAL_1_ARMIES;
 					}
@@ -236,15 +230,13 @@ public class CommandInput extends JPanel{
 						currentNeutralArmies = Data.NEUTRAL_4_ARMIES;
 					}
 
-
-
-
 					if(numReinforcementsToPlace <= currentNeutralArmies){
 						int currentUnits = Deck.countriesAfterShuffle[i].getPlayerArmies();
 						Deck.countriesAfterShuffle[i].setPlayerArmies(currentUnits+numReinforcementsToPlace);
 						CommandInput.appendStringTo((Deck.countriesAfterShuffle[i].getName()+" now has "+Deck.countriesAfterShuffle[i].getPlayerArmies() + " units\n"), Color.BLACK);
 
 						String nextNeutral = "";
+						//Series of if statements corresponding to each neutral player. Calculates reinforcement placings etc.
 						if(currentNeutral.compareToIgnoreCase("Neutral 1")==0){
 							Data.NEUTRAL_1_ARMIES-=numReinforcementsToPlace;
 							CommandInput.appendStringTo((currentNeutral + " now has "+ Data.NEUTRAL_1_ARMIES + " armies left.\n"), Color.BLUE);
@@ -306,51 +298,47 @@ public class CommandInput extends JPanel{
 
 	}
 
+	//getters for player 1 string, player 2 string, player 1 colour and player 2 colour.
 	public static String getPlayer1() {
 		return player1;
 	}
-
 	public static String getPlayer2(){
 		return player2;
 	}
-
 	public static Color getPlayer1Colour(){
 		return player1Colour;
 	}
-
 	public static Color getPlayer2Colour(){
 		return player2Colour;
 	}
 
-	public static void randomPlayerGenerator(String player1, String player2){
-		/*
-		 * random generator for who plays first.
-		 */
+	public static void randomPlayerGenerator(String player1, String player2){ // random generator for who plays first.
+		 
 		DiceRoll bothPlayers = new DiceRoll();
 		bothPlayers.rollDice();
 
-		appendStringTo(player1 + " rolled a " + bothPlayers.getPlayer1RollValue() + "\n", player1Colour);
-		appendStringTo(player2 + " rolled a " + bothPlayers.getPlayer2RollValue() + "\n", player2Colour);
+		appendStringTo(player1 + " rolled a " + DiceRoll.getPlayer1RollValue() + "\n", player1Colour);
+		appendStringTo(player2 + " rolled a " + DiceRoll.getPlayer2RollValue() + "\n", player2Colour);
 
 		checkIfDieEqual = "NO";
 
-		if(bothPlayers.rollWinnerOutcome() == "1" ){
+		if(bothPlayers.rollWinnerOutcome() == "1" ){ //If player 1 wins ..
 			appendStringTo((player1) + " to go first.\n", player1Colour);
 			currentPlayer = player1;
 		}
 
-		if(bothPlayers.rollWinnerOutcome() == "2" ){
+		if(bothPlayers.rollWinnerOutcome() == "2" ){ //If player 2 wins .. 
 			appendStringTo((player2) + " to go first.\n", player2Colour);
 			currentPlayer = player2;
 		}
 
-		if(bothPlayers.rollWinnerOutcome() == "3"){
+		if(bothPlayers.rollWinnerOutcome() == "3"){ //If the two die's rolled were equal .. 
 			appendStringTo((player1) + "'s and " + (player2) + "'s Die rolls were equal, Re-Roll required: \n", Color.RED);
 			checkIfDieEqual = "YES";
 		}
 	}
 
-	public static void appendStringTo(String s, Color c){
+	public static void appendStringTo(String s, Color c){ //append string function called every time a message is printed to the text field
 		//Add the ">" too each line in Dark Gray.
 		StyleConstants.setForeground(style, Color.DARK_GRAY);
 		try {
