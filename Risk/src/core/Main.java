752493ce6d4709ecc63fd4fc7cc982903e913495
Gregory <gregorysloggett@gmail.com>
2016-03-28 15:36:58 +0100

    Main function commented and tidied slightly..
    Signed-off-by: Gregory <gregorysloggett@gmail.com>

--- Risk/src/core/Main.java
+++ Risk/src/core/Main.java
@@ -2,7 +2,6 @@
  * Alan Halvey - 14465722
  * Alan Holmes - 14719591
  * Greg Sloggett - 14522247
- * 
  */
 package core;
 import java.awt.Color;
@@ -12,6 +11,7 @@ public class Main {
 
 	@SuppressWarnings("unused")
 	public static void main(String args[]) throws IOException{
+		
 		Screen screen = new Screen();
 		CommandInput.run();
 		DisplayInfo();
@@ -19,23 +19,25 @@ public class Main {
 		Screen.mainFrame.repaint();
 		//PlaceUnits();
 		Gameplay.calculateReinforcements();
+		
 		while(!(Data.Player1Wins || Data.Player2Wins)){
 			TurnSequence();
 			ChangePlayers();
 		}
-
 	}
 
+	//Changes the player name in the currentPlayer variable, which is used throughout game.
 	private static void ChangePlayers() {
+		
 		if(CommandInput.currentPlayer.compareTo(CommandInput.player1)==0){
 			CommandInput.currentPlayer = CommandInput.player2;
 		}
 		else{
 			CommandInput.currentPlayer = CommandInput.player1;
 		}
-		
 	}
 
+	//Displays the usernames and 1st player to start game. 
 	private static void DisplayInfo() {
 		boolean flag = true;
 
@@ -50,15 +52,14 @@ public class Main {
 
 		CommandInput.appendStringTo(CommandInput.player1 + " owns the following countries: \n" + Deck.player1Countries + "\n", CommandInput.player1Colour);
 		CommandInput.appendStringTo(CommandInput.player2 + " owns the following countries: \n" + Deck.player2Countries + "\n", CommandInput.player2Colour);
-
 		CommandInput.randomPlayerGenerator(CommandInput.player1, CommandInput.player2);
+		
 		while(CommandInput.checkIfDieEqual == "YES"){
 			CommandInput.randomPlayerGenerator(CommandInput.player1, CommandInput.player2);
 		}
-
 	}
 
-	private static void PlaceUnits() {
+/*	private static void PlaceUnits() {
 		while(Data.unitsLeft==true){
 			if(Data.PLAYER_1_ARMIES != 0 || Data.PLAYER_2_ARMIES != 0){
 				CommandInput.placeUnits(CommandInput.currentPlayer);
@@ -68,11 +69,10 @@ public class Main {
 				CommandInput.appendStringTo("No units left to place\n", Color.RED);
 				Data.unitsLeft=false;
 			}
-
 		}
+	}*/
 
-	}
-
+	//Sequence of functions for each player turn. 
 	private static void TurnSequence() {
 		System.out.println(Deck.countriesAfterShuffle[0].getName() + Deck.countriesAfterShuffle[0].getOccupyingPlayer().playerName + " " +Deck.countriesAfterShuffle[0].getOccupyingPlayer().numTerritories);
 		PlaceReinforcements();
@@ -80,6 +80,7 @@ public class Main {
 		Fortify();
 	}
 
+	//If current player has reinforcements left and it is his/her go, then they place their reinforcements.
 	private static void PlaceReinforcements() {
 		if(Gameplay.reinforcementsLeft(CommandInput.player1)==true && CommandInput.currentPlayer.compareTo(CommandInput.player1)==0){
 			Gameplay.placeReinforcements(CommandInput.currentPlayer);
@@ -96,9 +97,12 @@ public class Main {
 		Screen.mainFrame.repaint();
 	}
 
+	//Combat function allows attacking player to attack adjacent countries, based on a dice roll. #Risky
 	private static void Combat() {
 		CommandInput.appendStringTo("Would you like to combat? (Y/N)\n", Color.RED);
+		//If user enters no, he skips combat, moving onto Foritfy.
 		String choice = CommandInput.getCommand();
+		
 		if(choice.compareToIgnoreCase("Y")==0){
 			Gameplay.combat(CommandInput.currentPlayer);
 		}
@@ -109,22 +113,27 @@ public class Main {
 			CommandInput.appendStringTo("Invalid input. Try again.\n", Color.RED);
 			Combat();
 		}
-
 	}
+	
+	//Fortify function allows a player with adjacent countries to move armies between their territories. Only once per turn.
 	private static void Fortify() {
 		CommandInput.appendStringTo("Would you like to Fortify? (Y/N)\n", Color.RED);
+		//If user enters no, he skips fortify, moving onto the next players go.
 		String choice = CommandInput.getCommand();
 
 		if(choice.compareToIgnoreCase("Y")==0){
 			CommandInput.appendStringTo(CommandInput.currentPlayer+" Enter country to fortify armies from\n", Color.RED);
 			String takeArmies = CommandInput.getCommand();
 			CommandInput.appendStringTo(Gameplay.setFromAbbreviation(takeArmies)+"\n", CommandInput.currentPlayerColour);
+			
 			CommandInput.appendStringTo(CommandInput.currentPlayer+" Enter country to fortify armies to\n", Color.RED);
 			String putArmies = CommandInput.getCommand();
 			CommandInput.appendStringTo(Gameplay.setFromAbbreviation(putArmies)+"\n", CommandInput.currentPlayerColour);
+			
 			CommandInput.appendStringTo(CommandInput.currentPlayer+" Enter amount of armies to fortify\n", Color.RED);
 			int toMove = Integer.parseInt(CommandInput.getCommand());
 			CommandInput.appendStringTo( toMove+ " armies to fortify\n", Color.RED);
+			
 			takeArmies = Gameplay.setFromAbbreviation(takeArmies);
 			putArmies = Gameplay.setFromAbbreviation(putArmies);
 
