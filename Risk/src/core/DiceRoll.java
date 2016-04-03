bf341931c3397008458feef9e3cea51eea363ca9
Gregory <gregorysloggett@gmail.com>
2016-04-03 11:25:42 +0100

    Signed-off-by: Gregory <gregorysloggett@gmail.com>

--- Risk/src/core/DiceRoll.java
+++ Risk/src/core/DiceRoll.java
@@ -1,15 +1,11 @@
-
-
 /*
  * Alan Halvey - 14465722
  * Alan Holmes - 14719591
  * Greg Sloggett - 14522247
- * 
  */
 package core;
 
 import java.awt.Color;
-
 
 class Dice {
 	private int faceValue;
